76e6f6e0eaf3ae6e0630fa9c4afa0bb0170cb63b
VrajKoladiya <vrajkoladiya07work@gmail.com>
2024-05-24 11:40:41 +0530

    Add files via upload

---
+++ 1_2.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+main()
+{
+	printf("Name : Vraj Koladiya\n");
+	printf("Age : 17\n");
+	printf("School : Ashadeep 1\n");
+	printf("hobbi : Playing Cricket\n\n");
+	printf(" - - - \n");
+	printf("|     |\n");
+	printf("V     |\n");
+	printf("R     |\n");
+	printf("A     |\n");
+	printf("J     |\n");
+	printf("|     |\n");
+	printf(" - - -\n\n");
+	printf("*\n");
+	printf("* *\n");
+	printf("* * *\n");
+	printf("* *\n");
+	printf("*\n\n");
+	printf("*\n");
+	printf("*\n");
+	printf("*\t\t\t    * *\n");
+	printf("*\t\t\t  *\t*\n");
+	printf("*\t* *\t\t*\n");
+	printf("*     *     *         *\n");
+	printf("*   *         *     *\n");
+	printf("* *\t\t* *\n");
+	printf("*\n");
+}
